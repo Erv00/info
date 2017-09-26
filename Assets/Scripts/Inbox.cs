f8d2d5c59ef1dffc5c9011c6f0a76aec543d29c7
Erv00 <pek.erv00@gmail.com>
2017-09-26 20:05:55 +0200

    Added Inbox

---
+++ Assets/Scripts/Inbox.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inbox : MonoBehaviour {
+
+    int[] inbox;
+    #region basic
+    // Use this for initialization
+    void Start () {
+        inbox = GenerateNumericInbox(5, 1, 10); 
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    #endregion
+
+
+    private int[] GenerateNumericInbox(int length,int min,int max)
+    {
+        Random r = new Random();
+        int[] tmp = new int[length];
+        for(int i = 0; i < length; i++)
+        {
+            tmp[i] = Random.Range(min, max + 1);
+        }
+        PrintArray(tmp);
+        return(tmp);
+    }
+
+    #region debug
+    private void PrintArray(int[] arr)
+    {
+        for(int i = 0; i < arr.Length; i++)
+        {
+            Debug.Log(arr[i]);
+        }
+    }
+    #endregion
+
+}
