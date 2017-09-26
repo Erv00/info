a1a642975c96afc2285812a12173f5ff8e26e519
Erv00 <pek.erv00@gmail.com>
2017-09-26 19:50:20 +0200

    Basic Drag&Drop
    Working but needs refinements

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler,IDragHandler,IEndDragHandler{
+    public bool validDrop = false;
+    private bool isClone = false;
+
+    #region basi
+    // Use this for initialization
+    void Start () {
+        instance = gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    #endregion
+
+    public Transform parentToReturn = null;
+    GameObject instance = null;
+
+    public void OnBeginDrag(PointerEventData data)
+    {
+        #region Create Placeholder
+        //Create placeholder
+        if (!isClone)
+        {
+            GameObject tmp = Instantiate(instance);
+            tmp.transform.SetParent(transform.parent);
+            tmp.transform.SetSiblingIndex(transform.GetSiblingIndex());
+        }
+        isClone = true;
+        #endregion
+
+
+        parentToReturn = transform.parent;          //Set parent to return to
+        transform.SetParent(parentToReturn.parent); //Set parent as parent of parent
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;     //Enables raycasting
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        transform.position = data.position;
+    }
+
+    public void OnEndDrag(PointerEventData data)
+    {
+
+        if (!validDrop)
+        {
+            Debug.LogWarning("INVALID DROP");
+            Delete();
+        }
+        else
+        {
+            transform.SetParent(parentToReturn);        //Set parent to previous parent
+            GetComponent<CanvasGroup>().blocksRaycasts = true;     //Disables raycasting
+            validDrop = false;
+        }
+    }
+
+    private void Delete()
+    {
+        Destroy(gameObject);
+    }
+
+    public void SetClone()
+    {
+        isClone = true;
+    }
+
+}
---
+++ Assets/Scripts/DropZone.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DropZone : MonoBehaviour,IDropHandler {
+
+#region basic
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    #endregion
+
+    public enum ZoneType {Delete,Normal}
+    [SerializeField]
+    private ZoneType Type = ZoneType.Normal;
+
+    public void OnDrop(PointerEventData data)
+    {
+        //Preform checking
+        if(Type == ZoneType.Delete)
+        {
+            Destroy(data.pointerDrag.gameObject);
+            return;
+        }
+
+        data.pointerDrag.GetComponent<Draggable>().validDrop = true;
+        data.pointerDrag.GetComponent<Draggable>().parentToReturn = transform;
+    }
+
+}
